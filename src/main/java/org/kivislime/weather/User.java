ff08cf8d07d52524f67f86b2632a4d10c9805bcd
spinachh <kivislime@HypocritPc.>
2025-05-18 23:49:10 +0700

    fix: correct table/column names and adjust user→locations relationship

--- src/main/java/org/kivislime/weather/User.java
+++ src/main/java/org/kivislime/weather/User.java
@@ -13,18 +13,18 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     //TODO: размер в конфиг
-    @Column(name = "Login", unique = true, nullable = false, length = 100)
+    @Column(name = "login", unique = true, nullable = false, length = 100)
     private String login;
     //TODO: шифрование BCrypt
-    @Column(name = "Password", unique = true, nullable = false, length = 100)
+    @Column(name = "password", unique = true, nullable = false, length = 100)
     private String password;
     //TODO: cascade = CascadeType.ALL, orphanRemoval = true?
     @OneToMany(mappedBy = "user")
-    List<Locations> users;
+    List<Location> users;
 }
