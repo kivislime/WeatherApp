ec7bcecb3050e5a89503e5144e919619cd81dc68
spinachh <kivislime@HypocritPc.>
2025-05-26 00:22:52 +0700

    feat(auth): add AuthRedirectFilter and update SessionController, CookieFactory, CookieProperties

---
+++ src/main/java/org/kivislime/weather/CookieProperties.java
@@ -0,0 +1,19 @@
+package org.kivislime.weather;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CookieProperties {
+
+    private final String cookieName;
+    private final int cookieMaxAge;
+
+    public CookieProperties(@Value("${cookie.name:SESSION_ID}") String cookieName,
+                            @Value("${cookie.max-age.seconds:3600}") int cookieMaxAge) {
+        this.cookieName = cookieName;
+        this.cookieMaxAge = cookieMaxAge;
+    }
+}
