acffb2d06b31b9c15006a7684c01d4723b35514b
spinachh <kivislime@HypocritPc.>
2025-05-30 18:44:56 +0700

    refactor(repository): align repository with updated service logic

--- src/main/java/org/kivislime/weather/repository/LocationRepository.java
+++ src/main/java/org/kivislime/weather/repository/LocationRepository.java
@@ -1,11 +1,18 @@
 package org.kivislime.weather.repository;
 
 import org.kivislime.weather.entity.Location;
+import org.kivislime.weather.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface LocationRepository extends JpaRepository<Location, Long>{
+public interface LocationRepository extends JpaRepository<Location, Long> {
     List<Location> findByUserId(Long userId);
+
+    Optional<Location> findByUserIdAndLatitudeAndLongitude(Long userId, Double latitude, Double longitude);
+
+    long countByUser(User user);
+
     long deleteByUserIdAndLatitudeAndLongitude(Long user_id, Double latitude, Double longitude);
 }
