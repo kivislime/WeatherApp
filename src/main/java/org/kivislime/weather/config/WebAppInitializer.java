15d86fba8df7ca0d382eb083097b2cf2d276b5c7
spinachh <kivislime@HypocritPc.>
2025-06-14 14:23:23 +0700

    feat(init): programmatically set spring.profiles.active in WebAppInitializer

--- src/main/java/org/kivislime/weather/config/WebAppInitializer.java
+++ src/main/java/org/kivislime/weather/config/WebAppInitializer.java
@@ -1,9 +1,26 @@
 package org.kivislime.weather.config;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.checkerframework.checker.units.qual.N;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(@NonNull ServletContext servletContext) throws ServletException {
+        String profiles = System.getProperty("spring.profiles.active");
+        if (profiles == null || profiles.isEmpty()) {
+            profiles = System.getenv("SPRING_PROFILES_ACTIVE");
+        }
+        if (profiles != null && !profiles.isEmpty()) {
+            servletContext.setInitParameter("spring.profiles.active", profiles);
+        } else {
+            servletContext.setInitParameter("spring.profiles.active", "dev");
+        }
+        super.onStartup(servletContext);
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
